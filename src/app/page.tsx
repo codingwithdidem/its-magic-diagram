e7bd0cacc0d2f5d730f8163d27c528f0d7235045
didemkkasslan <didem@spiky.ai>
2023-06-02 16:25:55 +0300

    Create magic eraser card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useCallback } from "react";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
 
 export default function Home() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex min-h-screen flex-col bg-background items-center justify-center">
+      <div className="bg-[#1d1c20] min-h-[420px] max-h-[450px] w-[602px] border border-white border-opacity-[0.08] rounded-3xl p-8 overflow-hidden relative">
+        {/* Card Info */}
+        <div className="z-[100] relative max-w-[400px] text-white">
+          <h3 className="mb-2 font-semibold text-xl leading-6">
+            Wave goodbye to Lorem Ipsum
+          </h3>
+          <p className="text-[#a9a9a9] text-[15px] font-medium leading-6 mb-[10px]">
+            Magic Copy writes, edits, and rewrites Figma text layers so you can
+            design with real copy.
+          </p>
+        </div>
+
+        {/* Eraser */}
+        <div className="z-10 h-full relative top-[-64px] flex">
+          <div className="flex w-[500px] h-[56px] rounded-[10px] absolute top-[286px] right-[-52px] shadow-wand scale-[0.8] -rotate-[69deg]">
+            <div className="relative z-[5] bg-wand-shaft w-full flex justify-center flex-col items-end">
+              <div className="w-[430px] h-1 bg-wand-streak filter blur absolute mr-[-16px] mb-[6px]" />
+            </div>
+            <div className="z-[5] w-[140px] bg-wand-tip rounded-tr-[14px] rounded-br-[14px]" />
+          </div>
+        </div>
+
+        {/* Lorem Ipsum */}
+        <div className="z-[5] h-full w-full flex flex-col justify-center items-center absolute top-0 left-0 bottom-0 r-[-16px] overflow-hidden">
+          {/* Lorem Text */}
+          <div className="absolute top-[175px] right-[-116px] w-[684px] max-w-none text-[#272729] bg-[#323238] text-[102px] bg-clip-text font-bold [-webkit-text-fill-color:transparent] tracking-[-3px]">
+            Lorem Ipsum
+          </div>
+          {/* Particles blur */}
+          <div className="z-[2] overflow-hidden w-[684px] h-[135px] relative r-[0%] top-[98px] left-[62px] bottom-[0%] shadow-particle-blur"></div>
+
+          {/* Particles */}
+          <div className="z-[1] w-[684px] h-[135px] relative top-[-37px] bottom-[0%] left-[62px] right-[0%] overflow-hidden opacity-80">
+            <img
+              loading="eager"
+              src="https://uploads-ssl.webflow.com/6408bea3de5aef58b7e197d4/6418f6e0109266f276d32960_stars.svg"
+              className="z-[4] w-[1660px] max-w-none absolute top-[-111px] bottom-auto left-auto right-[-325px]"
             />
-          </a>
+
+            <Particles
+              id="tsparticles"
+              init={particlesInit}
+              loaded={particlesLoaded}
+              className="w-full h-[270px]"
+              options={{
+                fpsLimit: 120,
+                particles: {
+                  color: {
+                    value: "#ffffff",
+                  },
+                  collisions: {
+                    enable: false,
+                  },
+                  move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                      default: "destroy",
+                    },
+                    random: true,
+                    speed: 0.9,
+                    straight: false,
+                  },
+                  number: {
+                    density: {
+                      enable: true,
+                      area: 800,
+                    },
+                    value: 300,
+                  },
+                  opacity: {
+                    value: 0.4,
+                  },
+                  shape: {
+                    type: "circle",
+                  },
+                  size: {
+                    value: { min: 1, max: 2 },
+                  },
+                },
+                detectRetina: true,
+                fullScreen: false,
+              }}
+            />
+          </div>
+          <div></div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        background: "#131316",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "wand-tip":
+          "linear-gradient(#cfdcf1 1%,#fff 13%,#fff 59%,#dfe6f1 92%,#d4ddec 96%)",
+        "wand-streak": "linear-gradient(91deg,#000,#fff 31%)",
+        "wand-shaft": "linear-gradient(90deg,#0c0c0c,#1d1a1f)",
+      },
+
+      boxShadow: {
+        wand: "0 10px 16px rgba(0,0,0,.4)",
+        "particle-blur": "inset 0 2px 20px 20px #1d1c20",
       },
     },
   },
   plugins: [],
-}
+};
