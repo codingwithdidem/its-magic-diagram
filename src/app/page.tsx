5a96faada2ec4aa9ab9249c6de99cdbfbea7a02e
didemkkasslan <didem@spiky.ai>
2023-06-03 18:40:25 +0300

    Implement magic card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,60 @@
 "use client";
-import Image from "next/image";
-import { useCallback } from "react";
+
+import { MouseEvent, useCallback } from "react";
 import Particles from "react-particles";
 import type { Container, Engine } from "tsparticles-engine";
+import {
+  motion,
+  useMotionValue,
+  useMotionTemplate,
+  useTransform,
+} from "framer-motion";
 import { loadFull } from "tsparticles";
 
 export default function Home() {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
+  let mouseX = useMotionValue(-50);
+  let mouseY = useMotionValue(-50);
 
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
+  // - 50 is magic number used for mouseleave
+
+  // x en son -1 ve 601 oluyor
+  let clientX = useTransform(
+    mouseX,
+    [-50, 42, 560, 601],
+    [-470, -514, 250, 285]
+  );
+  let clientY = useTransform(mouseY, [-50, -1, 419], [41, -80, 23]);
+  let rotateZ = useTransform(mouseX, [-1, 601], [-25, 0]);
+
+  let loremContainerX = useTransform(
+    mouseX,
+    [-50, 70, 560, 621],
+    [10, 0, 650, 700]
+  );
+  let loremX = useTransform(mouseX, [-50, 70, 560, 601], [-10, 0, -650, -700]);
+
+  let magicContainerX = useTransform(
+    mouseX,
+    [-50, 270, 370, 521],
+    [-800, -320, -180, 0]
+  );
+
+  let magicX = useTransform(mouseX, [-50, 270, 370, 521], [800, 320, 180, 0]);
+
+  const handleMouseMove = ({ clientX, clientY, currentTarget }: MouseEvent) => {
+    let { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+
+    console.log({
+      x: clientX - left,
+
+      y: clientY - top,
+    });
+  };
+
+  const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
   }, []);
 
@@ -24,7 +67,14 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col bg-background items-center justify-center">
-      <div className="bg-[#1d1c20] min-h-[420px] max-h-[450px] w-[602px] border border-white border-opacity-[0.08] rounded-3xl p-8 overflow-hidden relative">
+      <div
+        onMouseMove={handleMouseMove}
+        onMouseLeave={() => {
+          mouseX.set(-50);
+          mouseY.set(-50);
+        }}
+        className="bg-[#1d1c20] min-h-[420px] max-h-[450px] w-[602px] border border-white border-opacity-[0.08] rounded-3xl p-8 overflow-hidden relative"
+      >
         {/* Card Info */}
         <div className="z-[100] relative max-w-[400px] text-white">
           <h3 className="mb-2 font-semibold text-xl leading-6">
@@ -37,26 +87,84 @@ export default function Home() {
         </div>
 
         {/* Eraser */}
-        <div className="z-10 h-full relative top-[-64px] flex">
+        <motion.div
+          style={{
+            willChange: "transform",
+            transition: "transform 0.1s linear",
+            transform: useMotionTemplate`translate3d(${clientX}px, ${clientY}px, 0px)
+            rotateZ(${rotateZ}deg)
+            `,
+            transformStyle: "preserve-3d",
+          }}
+          className="z-10 h-full relative top-[-64px] flex"
+        >
           <div className="flex w-[500px] h-[56px] rounded-[10px] absolute top-[286px] right-[-52px] shadow-wand scale-[0.8] -rotate-[69deg]">
             <div className="relative z-[5] bg-wand-shaft w-full flex justify-center flex-col items-end">
               <div className="w-[430px] h-1 bg-wand-streak filter blur absolute mr-[-16px] mb-[6px]" />
             </div>
             <div className="z-[5] w-[140px] bg-wand-tip rounded-tr-[14px] rounded-br-[14px]" />
           </div>
-        </div>
+        </motion.div>
+
+        {/* Its magical */}
+        <motion.div
+          className="z-[5] flex flex-col items-center justify-center h-full overflow-hidden absolute top-0 left-0 bottom-0 right-0 l-[16px]"
+          style={{
+            willChange: "transform",
+            transition: "transform 0.1s linear",
+            transform: useMotionTemplate`translate3d(${magicContainerX}px, 0px, 0px)`,
+          }}
+        >
+          <motion.img
+            className="w-[530px] relative top-[47px] right-[3px]"
+            src="https://uploads-ssl.webflow.com/6408bea3de5aef58b7e197d4/6412537be41c11523b118a34_It%E2%80%99s%20magical..svg"
+            style={{
+              willChange: "transform",
+              transition: "transform 0.1s linear",
+              transform: useMotionTemplate`translate3d(${magicX}px, 0px, 0px)`,
+            }}
+          />
+        </motion.div>
 
         {/* Lorem Ipsum */}
-        <div className="z-[5] h-full w-full flex flex-col justify-center items-center absolute top-0 left-0 bottom-0 r-[-16px] overflow-hidden">
+        <motion.div
+          style={{
+            willChange: "transform",
+            transition: "transform 0.1s linear",
+            transform: useMotionTemplate`translate3d(${loremContainerX}px, 0px, 0px)`,
+          }}
+          className="z-[5] h-full w-full flex flex-col justify-center items-center absolute top-0 left-0 bottom-0 r-[-16px] overflow-hidden"
+        >
           {/* Lorem Text */}
-          <div className="absolute top-[175px] right-[-116px] w-[684px] max-w-none text-[#272729] bg-[#323238] text-[102px] bg-clip-text font-bold [-webkit-text-fill-color:transparent] tracking-[-3px]">
+          <motion.div
+            style={{
+              willChange: "transform",
+              transition: "transform 0.1s linear",
+              transform: useMotionTemplate`translate3d(${loremX}px, 0px, 0px)`,
+            }}
+            className="absolute top-[161px] right-[-117px] w-[684px] max-w-none text-[#272729] bg-[#323238] text-[102px] bg-clip-text font-bold [-webkit-text-fill-color:transparent] tracking-[-3px]"
+          >
             Lorem Ipsum
-          </div>
+          </motion.div>
           {/* Particles blur */}
-          <div className="z-[2] overflow-hidden w-[684px] h-[135px] relative r-[0%] top-[98px] left-[62px] bottom-[0%] shadow-particle-blur"></div>
+          <motion.div
+            style={{
+              willChange: "transform",
+              transition: "transform 0.1s linear",
+              transform: useMotionTemplate`translate3d(${loremX}px, 0px, 0px)`,
+            }}
+            className="z-[2] overflow-hidden w-[684px] h-[135px] relative r-[0%] top-[98px] left-[62px] bottom-[0%] shadow-particle-blur"
+          ></motion.div>
 
           {/* Particles */}
-          <div className="z-[1] w-[684px] h-[135px] relative top-[-37px] bottom-[0%] left-[62px] right-[0%] overflow-hidden opacity-80">
+          <motion.div
+            style={{
+              willChange: "transform",
+              transition: "transform 0.1s linear",
+              transform: useMotionTemplate`translate3d(${loremX}px, 0px, 0px)`,
+            }}
+            className="z-[1] w-[684px] h-[135px] relative top-[-37px] bottom-[0%] left-[62px] right-[0%] overflow-hidden opacity-80"
+          >
             <img
               loading="eager"
               src="https://uploads-ssl.webflow.com/6408bea3de5aef58b7e197d4/6418f6e0109266f276d32960_stars.svg"
@@ -108,9 +216,9 @@ export default function Home() {
                 fullScreen: false,
               }}
             />
-          </div>
+          </motion.div>
           <div></div>
-        </div>
+        </motion.div>
       </div>
     </main>
   );
